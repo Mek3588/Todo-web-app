2a0099900d391b93c9d15abdcd5a1843f7cacd9c
Mekbib <87893278+Mek3588@users.noreply.github.com>
2022-10-14 23:13:40 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+
+from turtle import title
+from xmlrpc.client import Boolean
+from flask import Flask, render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+with app.app_context():
+    class Todo(db.Model):
+        id = db.Column(db.Integer,primary_key=True)
+        title = db.Column(db.String(100))
+        complete = db.Column(db.Boolean)
+
+
+
+
+
+    @app.route('/')
+    def index():
+        todo_list = Todo.query.all()
+        return render_template('base.html',todo_list=todo_list)
+
+    @app.route("/add",methods=["POST"])
+    def add():
+        title = request.form.get("title")
+        new_todo=Todo(title=title,complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+    @app.route("/Update/<int:todo_id>")
+    def Update(todo_id):
+        todo = Todo.query.filter_by(id=todo_id).first()
+        todo.complete = not todo.complete
+        db.session.commit()
+        return redirect(url_for("index"))
+
+
+    @app.route("/Delete/<int:todo_id>")
+    def Delete(todo_id):
+        todo = Todo.query.filter_by(id=todo_id).first()
+        db.session.delete(todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+
+
+
+
+    if __name__ =="__main__":
+        db.create_all()
+        app.run(debug=True)
